18da501f7c8fc6ca8550bd68576820463b30a20c
carlos42044 <carlos42044@gmail.com>
2016-10-15 18:36:56 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<title>Carlos Flores</title>
+	<head>
+		<link type="text/css" rel="stylesheet" href="css/style.css">
+	</head>
+
+	<body>
+		<h1>Welcome to my webssssite</h1>
+		<p class="hello">This is a test of what ftp can do</p>
+		<p>Just another test to see if file permissions worked</p>
+		<p></p>
+		<p class="hello">I learned some css but, how can i actually get it to work?</p>
+		<?php 
+			echo nl2br("Hello world!\nHello Cristen!\n");
+			echo date('Y-m-d H:i:s');
+		?>
+	</body>
+</html>
